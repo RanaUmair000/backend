03ca764a20fdc3648834b0b93a10150f2306964c
M Umair <ranaumairfyp@gmail.com>
2026-03-28 23:56:14 +0500

    ff

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use("/api/diary", diaryRoutes);
 app.use('/api/teacher-attendance', teacherAttendanceRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
